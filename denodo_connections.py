81f1f19f4faae9dcd8b90dcdcd6d8a024a7825ad
SAMAR SINGH <97422694+samarsinghchauhan@users.noreply.github.com>
2022-01-15 10:57:38 +0530

    Update denodo_connections.py

--- denodo_connections.py
+++ denodo_connections.py
@@ -7,19 +7,19 @@ details= {
 'server': 'deenodo.dev.corp
 'port' : '9996',
 'userid':'devuser',
-'password':'devpassword'
+'password':'xxxxxx'
 },
 preprod':{
 'odbcdriver' : 'DenodoODBC Unicode(x64)',
 'server': 'deenodo.preprod.corp
 'port' : '9996',
 'userid':'devuser',
-'password':'devpassword'
+'password':'xxxxxx'
 },
 'prod':{
 'odbcdriver' : 'DenodoODBC Unicode(x64)',
 'server': 'deenodo.prod.corp
 'port' : '9996',
 'userid':'devuser',
-'password':'devpassword'
+'password':'xxxxxx'
 }
